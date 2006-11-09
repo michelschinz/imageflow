54c51c294c602ca32822de3cb394cb91e210bb7c
Michel Schinz <Michel.Schinz@gmail.com>
2006-11-09 11:34:06 +0200

    Ajouté la fonction union à Mlist

--- ImageFlow/ocaml/evaluator/mlist.ml
+++ ImageFlow/ocaml/evaluator/mlist.ml
@@ -2,3 +2,10 @@ let rec remove_first e = function
     [] -> []
   | hd :: tl when hd = e -> tl
   | hd :: tl -> hd :: (remove_first e tl)
+
+let rec union l1 l2 = match l1, l2 with
+  l1, [] -> l1
+| [], l2 -> l2
+| h1 :: t1, l2 ->
+    if List.mem h1 l2 then union t1 l2 else union t1 (h1 :: l2)
+
--- ImageFlow/ocaml/evaluator/mlist.mli
+++ ImageFlow/ocaml/evaluator/mlist.mli
@@ -1,1 +1,2 @@
 val remove_first : 'a -> 'a list -> 'a list
+val union : 'a list -> 'a list -> 'a list
