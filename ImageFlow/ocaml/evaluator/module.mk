cd916b0feb2510d6efce3e89758644139163e583
Michel Schinz <michel.schinz@gmail.com>
2010-11-09 21:26:12 +0100

    Overhaul building of OCaml code

--- ImageFlow/ocaml/evaluator/module.mk
+++ ImageFlow/ocaml/evaluator/module.mk
@@ -1,31 +1,23 @@
-LOCAL_SRC :=
-
-# Debugging support
-LOCAL_SRC += log.ml log.mli
-
-# Additional library functions
-LOCAL_SRC += marray.ml marray.mli
-LOCAL_SRC += mlist.ml mlist.mli
-
-# Evaluator sources
-LOCAL_SRC += blendmode.ml blendmode.mli
-LOCAL_SRC += point.ml point.mli
-LOCAL_SRC += color.ml color.mli
-LOCAL_SRC += interval.ml interval.mli
-LOCAL_SRC += size.ml size.mli
-LOCAL_SRC += rect.ml rect.mli
-LOCAL_SRC += image.ml image.mli
-LOCAL_SRC += affinetransform.ml affinetransform.mli
-LOCAL_SRC += coreimage.ml coreimage.mli
-LOCAL_SRC += expr.ml expr.mli
-LOCAL_SRC += printer.ml printer.mli
-LOCAL_SRC += optimiser.ml optimiser.mli
-LOCAL_SRC += type.ml type.mli
-LOCAL_SRC += typechecker.ml typechecker.mli
-LOCAL_SRC += evaluator.ml evaluator.mli
-LOCAL_SRC += cache.ml cache.mli
-LOCAL_SRC += optevaluator.ml optevaluator.mli
-LOCAL_SRC += delta.ml delta.mli
-LOCAL_SRC += registerer.ml
-
-SRC += $(foreach local_file,$(LOCAL_SRC),evaluator/$(local_file))
+SRC += evaluator/affinetransform.ml evaluator/affinetransform.mli
+SRC += evaluator/blendmode.ml evaluator/blendmode.mli
+SRC += evaluator/cache.ml evaluator/cache.mli
+SRC += evaluator/color.ml evaluator/color.mli
+SRC += evaluator/coreimage.ml evaluator/coreimage.mli
+SRC += evaluator/delta.ml evaluator/delta.mli
+SRC += evaluator/evaluator.ml evaluator/evaluator.mli
+SRC += evaluator/expr.ml evaluator/expr.mli
+SRC += evaluator/image.ml evaluator/image.mli
+SRC += evaluator/interval.ml evaluator/interval.mli
+SRC += evaluator/log.ml
+SRC += evaluator/marray.ml evaluator/marray.mli
+SRC += evaluator/mlist.ml evaluator/mlist.mli
+SRC += evaluator/optevaluator.ml evaluator/optevaluator.mli
+SRC += evaluator/optimiser.ml evaluator/optimiser.mli
+SRC += evaluator/point.ml evaluator/point.mli
+SRC += evaluator/primitives.ml evaluator/primitives.mli
+SRC += evaluator/printer.ml evaluator/printer.mli
+SRC += evaluator/rect.ml evaluator/rect.mli
+SRC += evaluator/registerer.ml
+SRC += evaluator/size.ml evaluator/size.mli
+SRC += evaluator/type.ml evaluator/type.mli
+SRC += evaluator/typechecker.ml evaluator/typechecker.mli
--- ImageFlow/ocaml/evaluator/operators/module.mk
+++ ImageFlow/ocaml/evaluator/operators/module.mk
@@ -1,6 +1,3 @@
-SRC += evaluator/operators/export.m
-SRC += evaluator/operators/export.ml
-SRC += evaluator/operators/export.mli
-SRC += evaluator/operators/paint.m
-SRC += evaluator/operators/paint.ml
-SRC += evaluator/operators/paint.mli
+SRC += evaluator/operators/export.ml evaluator/operators/export.mli
+SRC += evaluator/operators/paint.ml evaluator/operators/paint.mli
+SRC += evaluator/operators/save.ml evaluator/operators/save.mli
